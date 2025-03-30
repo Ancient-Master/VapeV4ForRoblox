6aedbd41cccf7c96afda00b8908bb4e97f5fd3be
Ancient-Master <81575452+Ancient-Master@users.noreply.github.com>
2025-03-30 21:30:48 +0200

    Update 82723494568572.lua

--- games/82723494568572.lua
+++ games/82723494568572.lua
@@ -7,3 +7,18 @@ local test = vape.Categories.Combat:CreateModule({
     end,
     Tooltip = 'This is a test module'
 })
+
+local Affe
+Affe = vape.Categories.Combat:CreateModule({
+	Name = 'teststuff',
+	Function = function(callback)
+		print(callback, 'module state')
+		Affe:Clean(Instance.new('Part'))
+		repeat
+			print('repeat loop!')
+			task.wait(1)
+		until (not Affe.Enabled)
+	end,
+	ExtraText = function() return 'Test' end,
+	Tooltip = 'This is a test module.'
+})

879abab95da8c07ac23b80c96cb65464d9ac260e
Ancient-Master <81575452+Ancient-Master@users.noreply.github.com>
2025-03-30 21:25:18 +0200

    Create 82723494568572.lua

---
+++ games/82723494568572.lua
@@ -0,0 +1,14 @@
+local SilentAim
+SilentAim = vape.Categories.Combat:CreateModule({
+	Name = 'SilentAim',
+	Function = function(callback)
+		print(callback, 'module state')
+		SilentAim:Clean(Instance.new('Part'))
+		repeat
+			print('repeat loop!')
+			task.wait(1)
+		until (not SilentAim.Enabled)
+	end,
+	ExtraText = function() return 'Test' end,
+	Tooltip = 'This is a test module.'
+})

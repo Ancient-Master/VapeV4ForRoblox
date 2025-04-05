d397d7b7a5dd83d706d953f65027571edbd7d628
Ancient-Master <81575452+Ancient-Master@users.noreply.github.com>
2025-04-05 18:50:01 +0200

    Update 132352755769957.lua

--- games/132352755769957.lua
+++ games/132352755769957.lua
@@ -66,12 +66,12 @@ local function checkForTarget()
             return target
         else
             notif('Vape', "Found wrong target: " .. target.player.Name, 3, 'warning')
-            task.wait(1)
+            task.wait()
             return checkForTarget()
         end
     else
         notif('Vape', "No target found, retrying...", 1, 'warning')
-        task.wait(1)
+        task.wait()
         return checkForTarget()
     end
 end
@@ -83,7 +83,7 @@ spin = vape.Categories.Combat:CreateModule({
             if not notp.Enabled then
                 originalPosition = LocalPlayer.Character.HumanoidRootPart.CFrame
                 LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(125.482315, 254.5, -749.594482, -0.00281787151, 1.3361479e-07, 0.999996006, 1.39850187e-10, 1, -1.33614932e-07, -0.999996006, -2.3666008e-10, -0.00281787151)
-                task.wait(0.5)
+                task.wait()
             end
 
             local target = checkForTarget()
@@ -100,7 +100,7 @@ spin = vape.Categories.Combat:CreateModule({
                         }
                         Namespaces.MeleeReplication.packets.sendHit.send(args)
                     end
-                    task.wait(0.1)
+                    task.wait()
                 until not target or not target.player or not target.player.Character or 
                       target.player.Character.Humanoid.Health <= 0 or 
                       not killa.Enabled or 

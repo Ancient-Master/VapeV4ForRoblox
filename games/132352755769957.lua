51f601cceb57f9ce5b5b3d222165508da4bed0fd
Ancient-Master <81575452+Ancient-Master@users.noreply.github.com>
2025-04-05 14:50:48 +0200

    Update 132352755769957.lua

--- games/132352755769957.lua
+++ games/132352755769957.lua
@@ -59,7 +59,6 @@ spin = vape.Categories.Combat:CreateModule({
 				originalPosition = LocalPlayer.Character.HumanoidRootPart.CFrame
 				LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(125.482315, 254.5, -749.594482, -0.00281787151, 1.3361479e-07, 0.999996006, 1.39850187e-10, 1, -1.33614932e-07, -0.999996006, -2.3666008e-10, -0.00281787151)
 				task.wait(0.5)
-				continue
 			end
 
             checkForTarget()
